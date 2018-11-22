cf87101ecaa48ae3e7624a8e5825f571e8bec72c
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-11-22 12:59:25 +0300

    Home implementation has been converted as a model view.

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Data;
+using MyPortfolio.Models.ViewModels.Home;
 
 namespace MyPortfolio.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HomeViewModel model = new HomeViewModel()
+            {
+                Resumes = await _context.Resumes.Where(r => r.IsActive == true).ToListAsync(),
+                Educations = await _context.Educations.OrderBy(e => e.StartedAt).ToListAsync(),
+                Experiences = await _context.Experiences.OrderBy(e => e.StartedAt).ToListAsync(),
+                Works = await _context.Works.OrderBy(e => e.StartedAt).ToListAsync(),
+                Contact = new Models.Contact()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ MyPortfolio/Models/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Resume> Resumes { get; set; }
+        public IEnumerable<Education> Educations { get; set; }
+        public IEnumerable<Experience> Experiences { get; set; }
+        public IEnumerable<Work> Works { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
