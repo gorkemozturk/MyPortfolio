05cb896b81465ae728f50ef31bea76b0e5eeeca2
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-11-22 13:35:08 +0300

    Show method that related with contact implemented in the management area.

---
+++ MyPortfolio/Areas/Management/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Data;
+using MyPortfolio.Utilities;
+
+namespace MyPortfolio.Areas.Management.Controllers
+{
+    [Area("Management")]
+    [Authorize(Roles = StaticEnvironments.Administrator)]
+    public class ContactController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await _context.Contacts.OrderByDescending(c => c.CreatedAt).ToListAsync();
+
+            return View(contacts);
+        }
+
+        public async Task<IActionResult> Show(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+                return NotFound();
+
+            return View(contact);
+        }
+    }
+}
